7c2cdc01156fe631cc47047fa142f5baa1543cc6
julieta011 <margaryanjulietta011@gmail.com>
2022-03-21 18:03:21 +0400

    Update RegisButton.js

--- src/components/RegisButton/RegisButton.js
+++ src/components/RegisButton/RegisButton.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import "../../assets/css/button.css"
 class RegisButton extends Component {
     render() {
+        const { backgroundColor, color, title, buttonIconColor, colorIcon, icon} = this.props;
         return (
-            <div className="button-regis" style={{backgroundColor:this.props.backgroundColor}} onClick={() => this.props.onChangeValue()}>
-                <div className="button-regis-text" style={{color:this.props.color}}>{this.props.title}</div>
-                <div className="button-icon" style={{backgroundColor:this.props.buttonIconColor}} >
-                    <div className="triangle1"></div>
-                    <div className="triangle2"></div>
-                    <i style={{color:this.props.colorIcon}} className={this.props.icon} aria-hidden="true"></i>
+            <div className="button-regis" style={{backgroundColor}} onClick={() => this.props.onChangeValue()}>
+                <div className="button-regis-text" style={{color}}>{title}</div>
+                <div className="button-icon" style={{backgroundColor:buttonIconColor}} >
+                    <div className="triangle1"/>
+                    <div className="triangle2"/>
+                    <i style={{color: colorIcon}} className={icon} aria-hidden="true"></i>
                 </div>
             </div>
         );
